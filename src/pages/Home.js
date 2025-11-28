452b76bdcdc459377c6ce746d67df3b612785409
Paulo Silva <paulofernando78@gmail.com>
2025-11-28 20:53:58 -0300

    wip

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,6 @@ class Home extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-
-      const style = document.createElement("style");
-      style.textContent = styleImports;
-      this.shadowRoot.appendChild(style);
-
-    this.container = document.createElement("div");
-    this.container.className = "line-break";
-    this.shadowRoot.appendChild(this.container);
   }
 
   connectedCallback() {
@@ -20,19 +12,20 @@ class Home extends HTMLElement {
 
   render() {
     /* html */
-    this.container.innerHTML = `
-      <h1>Boilerplate</h1>
-      <div>
-        <ul>
-          <li>• JS Vanilla</li>
-          <li>• Vite</li>
-          <li>• Web Components</li>
-        </ul>
-      </div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iste accusantium tempora in ad dolores quas, esse dolorem, odit eligendi temporibus beatae ut vel, quos fugit impedit rerum magni maiores.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iste accusantium tempora in ad dolores quas, esse dolorem, odit eligendi temporibus beatae ut vel, quos fugit impedit rerum magni maiores.</p>
-      <p>Lorem ipsum dolor sit amet...</p>
-      <p>Lorem ipsum dolor sit amet...</p>
+    this.shadowRoot.innerHTML = `
+      <style>${styleImports}</style>
+      <section class="line-break">
+        <h1 class="page">Boilerplate</h1>
+        <div>
+          <ul>
+            <li>• JS Vanilla</li>
+            <li>• Vite</li>
+            <li>• Web Components</li>
+          </ul>
+        </div>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iste accusantium tempora in ad dolores quas, esse dolorem, odit eligendi temporibus beatae ut vel, quos fugit impedit rerum magni maiores.</p>
+      </section>
     `;
   }
 }
